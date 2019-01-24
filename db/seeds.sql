b2fe9e4c70856bc278480990c59deb923a995d38
Jeff Lawlis <jeff.lawlis@yahoo.com>
2019-01-24 10:38:34 -0600

    actually added sql code

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (name) VALUES ('Western Bacon Chee');
+INSERT INTO burgers (name) VALUES ('Too Err is Cumin Burger');
+INSERT INTO burgers (name) VALUES ('The Grand Brie Burger');
+
